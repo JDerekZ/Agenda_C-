c8a6f9fc1e6d56570f5aedbda85775185c7dbd9a
JDerekZ <Jplasencia03@gmail.com>
2022-07-17 00:08:53 -0400

    Initial commit

---
+++ Dates_Layer/BD_Connect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Dates_Layer
+{
+    public class BD_Connect
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ConnectionString);
+
+        public SqlConnection OpensqlConnection()
+        {
+            if(connection.State==ConnectionState.Closed)
+                connection.Open();
+            return connection;
+        }
+
+        public SqlConnection ClosedsqlConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+            return connection;
+        }
+
+    }
+}
